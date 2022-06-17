4da530ac7fdf933bfed11cfec1fcc671cca7f9be
Martin Brilly <martin_brilly@yahoo.co.uk>
2022-06-17 13:39:03 +0000

    add math floor and if else statment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+
+            } else {
+                let gametype = this.getAttribute("data-type");
+                alert(`You clicked ${gametype}`);
+            }
+        })
+    }
+})
+
 function rungame() {
 
 }
